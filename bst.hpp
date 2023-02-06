8e754ad6c3252c75dad926acaeb54b36100536e9
Jordan King yen hii <jking-ye@u91z05s04.42kl.edu.my>
2023-02-06 15:30:18 +0800

    added allocator

--- bst.hpp
+++ bst.hpp
@@ -9,11 +9,21 @@ namespace ft
 	struct	node
 	{
 		node(){};
-            node(T key)
-            {
-                  this->key = key;
-            }
+			node(T key)
+			{
+				  this->key = key;
+			}
 		~node(){};
+			void  printdata(std::string name)
+			{
+				  std::cout << "----------------------------------" << std::endl;
+				  std::cout << "temp name of node   : " << name << std::endl;
+				  std::cout << "parent of node      : " << this->parent->key << std::endl;
+				  std::cout << "left child of node  : " << this->left_child->key << std::endl;
+				  std::cout << "right child of node : " << this->right_child->key << std::endl;
+				  std::cout << "key of node         : " << this->key << std::endl;
+				  std::cout << "----------------------------------" << std::endl;
+			}
 		
 		T		key;
 		node*	left_child;
@@ -21,33 +31,41 @@ namespace ft
 		node*	parent;
 	};
 
-      template <typename T>
+	template < typename T, class Compare = std::less<T> , class Allocator = std::allocator<T> >
 	class bst
 	{
 		public:
-            bst();
-            bst(const bst& clone);
-            ~bst();
+			typedef Allocator														allocator_type;
+			typedef	typename allocator_type::template rebind< node<T> >::other		node_allocator;
+			typedef	typename allocator_type::template rebind< T >::other			t_allocator;
 
-            void        insertnode(T key);
-            void        deletenode(T key);
-            void        bstwalk(node<T> *x);
-            node<T>     bstsearch(node<T> *x, T key);
-            node<T>     iterativebstsearch(node<T> *x, T key);
-            node<T>*     bstminimum(node<T> *x);
-            node<T>*     bstmaximum(node<T> *x);
-            node<T>*     bstsuccessor(node<T> *x);
-            void        bstinsert(node<T> *z);
-            void        bsttransplant(node<T> *u, node<T> *v);
-            void        bstdelete(node<T> *z);
-            void        printBT(const std::string& prefix, const node<T>* node, bool isLeft);
-            void        printBT(void);
-            void        bstclear(node<T> *x);
+			bst();
+			bst(const bst& clone);
+			~bst();
+
+			void		insertnode(T key);
+			void		deletenode(T key);
+			node<T>*	searchnode(T key);
+			node<T>*	iterativesearchnode(T key);
+			void		printBT(void);
+			node<T>*	bstsuccessor(node<T> *x);
 
 		private:
-            node<T> *_root;
-            node<T> *_nil;
+			void		bstwalk(node<T> *x);
+			node<T>*	bstsearch(node<T> *x, T key);
+			node<T>*	iterativebstsearch(node<T> *x, T key);
+			node<T>*	bstminimum(node<T> *x);
+			node<T>*	bstmaximum(node<T> *x);
+			void		bstinsert(node<T> *z);
+			void		bsttransplant(node<T> *u, node<T> *v);
+			void		bstdelete(node<T> *z);
+			void		printBT(const std::string& prefix, const node<T>* node, bool isLeft);
+			void		bstclear(node<T> *x);
 
+			node_allocator	n_alloc;
+			t_allocator		t_alloc;
+			node<T>			*_root;
+			node<T>			*_nil;
 	};
 }
 
--- map.cpp
+++ map.cpp
@@ -8,17 +8,23 @@ using std::endl;
 
 int main(void)
 {
-    ft::bst<int> tree;
+	ft::bst<int>	tree;
+	
+	tree.insertnode(9);
+	tree.insertnode(1);
+	tree.insertnode(10);
+	tree.insertnode(2);
+	tree.insertnode(1);
+	tree.insertnode(3);
+	// tree.deletenode(9);
+	// tree.deletenode(10);
+	// tree.deletenode(1);
+	// tree.deletenode(30);
 
-    tree.insertnode(9);
-    tree.insertnode(1);
-    tree.insertnode(10);
-    tree.insertnode(2);
-    tree.insertnode(1);
-    tree.insertnode(3);
-    tree.deletenode(9);
-    tree.deletenode(10);
-    tree.deletenode(30);
-    tree.printBT();
-    return (0);
+	ft::node<int> *p = tree.searchnode(1);
+	p->printdata("p");
+	tree.printBT();
+
+	// system("leaks con");
+	return (0);
 }
